845a3af15715dfe4f07e96855d6fe27ffe0d2ee6
geunhee <lee.geunheee@gmail.com>
2021-04-26 17:46:33 -0400

    essay first draft

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = choosePic;
 
-var myPix = new Array("images/light/DSC05848-2.jpg","images/light/DSC05634-HDR-Pano-2_low.jpg","images/light/DSC05848-2.jpg","images/light/DSC05941-Pano-2_low.jpg","images/light/DSC05947-Pano-3_low.jpg","images/detail/DSC06813-HDR.jpg","images/detail/DSC06813-HDR.jpg","images/detail/DSC06813-HDR.jpg","images/detail/MAH07232_poster.jpg","images/detail/MAH07232_poster.jpg","images/poetics/DSC07657.jpg","images/poetics/DSC07903-HDR.jpg","images/poetics/DSC07993.jpg","images/poetics/DSC08165.jpg","images/poetics/DSC08178.jpg");
+var myPix = new Array("images/light/MAH05886-2.jpg","images/light/DSC05848-2.jpg","images/light/DSC05634-HDR-Pano-2_low.jpg","images/light/DSC05941-Pano-2_low.jpg","images/light/DSC05943-Pano.jpg","images/detail/DSC06813-HDR.jpg","images/detail/DSC06813-HDR.jpg","images/detail/DSC06813-HDR.jpg","images/detail/MAH07232_poster.jpg","images/detail/MAH07232_poster.jpg","images/poetics/DSC07657.jpg","images/poetics/DSC07903-HDR.jpg","images/poetics/DSC07993.jpg","images/poetics/DSC08165.jpg","images/poetics/DSC08178.jpg");
 
 function choosePic() {
   var randomNum = Math.floor(Math.random() * myPix.length);
